40b59a8b282b505906f80e65720a7a11907b2155
Mathis Detourbet <mathis.detourbet@gmail.com>
2020-05-18 12:54:21 +0200

    STEP 1: Init and install Cocoapods. Xcode workspace created

---
+++ Pods/Target Support Files/Pods-FavQuotes-FavQuotesUITests/Pods-FavQuotes-FavQuotesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FavQuotes_FavQuotesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FavQuotes_FavQuotesUITests
+@end
---
+++ Pods/Target Support Files/Pods-FavQuotes/Pods-FavQuotes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FavQuotes : NSObject
+@end
+@implementation PodsDummy_Pods_FavQuotes
+@end
